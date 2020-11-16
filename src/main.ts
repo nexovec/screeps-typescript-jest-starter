4b7b51adf5a3e858a3f6bca8b7cfdbe64b802104
nexovec <hraccesky@gmail.com>
2020-11-16 01:05:40 +0100

    fix lodash version, generate pixel

--- src/main.ts
+++ src/main.ts
@@ -1,46 +1,45 @@
-import roleBuilder, { Builder } from 'roles/builder';
-import roleHarvester from 'roles/harvester';
-import roleUpgrader, { Upgrader } from 'roles/upgrader';
-import ErrorMapper from 'utils/ErrorMapper';
-import { runTower } from './tower';
-
+import { cpuUsage } from "process";
+import roleBuilder, { Builder } from "roles/builder";
+import roleHarvester from "roles/harvester";
+import roleUpgrader, { Upgrader } from "roles/upgrader";
+import ErrorMapper from "utils/ErrorMapper";
+import { runTower } from "./tower";
 
 function unwrappedLoop() {
   console.log(`Current game tick is ${Game.time}`);
 
-  Object.values(Game.rooms).forEach(room => {
+  Object.values(Game.rooms).forEach((room) => {
     if (room.controller?.my) {
       const towers = room.find<StructureTower>(FIND_MY_STRUCTURES, { filter: { structureType: STRUCTURE_TOWER } });
 
-      towers.forEach(tower => {
+      towers.forEach((tower) => {
         runTower(tower);
       });
     }
   });
 
-  Object.values(Game.creeps).forEach(creep => {
-    if (creep.memory.role === 'harvester') {
+  Object.values(Game.creeps).forEach((creep) => {
+    if (creep.memory.role === "harvester") {
       roleHarvester.run(creep);
     }
-    if (creep.memory.role === 'upgrader') {
+    if (creep.memory.role === "upgrader") {
       roleUpgrader.run(creep as Upgrader);
     }
-    if (creep.memory.role === 'builder') {
+    if (creep.memory.role === "builder") {
       roleBuilder.run(creep as Builder);
     }
   });
 
   // Automatically delete memory of missing creeps
   Object.keys(Memory.creeps)
-    .filter(name => !(name in Game.creeps))
-    .forEach(name => delete Memory.creeps[name]);
+    .filter((name) => !(name in Game.creeps))
+    .forEach((name) => delete Memory.creeps[name]);
+
+  if (Game.cpu.bucket > 9000) Game.cpu.generatePixel();
 }
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
 // This utility uses source maps to get the line numbers and file names of the original, TS source code
 const loop = ErrorMapper.wrapLoop(unwrappedLoop);
 
-export {
-  loop,
-  unwrappedLoop
-};
+export { loop, unwrappedLoop };
