75c6e07e6b4df6a8b5d11108b71dc1bdc8009f12
nexovec <hraccesky@gmail.com>
2021-01-17 22:05:18 +0100

    working construction site designation for RCL 2

--- src/Root.ts
+++ src/Root.ts
@@ -10,7 +10,6 @@ class Root {
 
   private sm: SourceManager;
 
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
   private constructor() {
     this.sm = new SourceManager(Game.spawns.Spawn1.room);
   }
@@ -21,22 +20,61 @@ class Root {
   }
 
   public loop() {
-    // TODO: beware, you're overloading the sources\
+    // TODO: beware, you're overloading the sources
     if (!Game.spawns.Spawn1.room?.controller?.level) console.log('wtf RCL 0??');
-    if ((Game.spawns.Spawn1.room.controller as StructureController).level >= 1) {
-    if (this.sm.allSlots + this.sm.sources.length * this.sm.reserve > Object.keys(Game.creeps).length) {
-      Game.spawns.Spawn1.spawnCreep([WORK, CARRY, MOVE], _.uniqueId('nex#'));
-    }
-    _.map(Game.creeps, (creep: Creep) => {
-      if (!creep.reserved) {
-        const s: SourceWrapper | null = this.sm.reserveSourceSlot() as SourceWrapper;
-        if (s) {
-          creep.reserved = true;
-          CreepActions.basicCreepHarvesting(creep.id, s);
-        }
+    const RCL = (Game.spawns.Spawn1.room.controller as StructureController).level;
+    const spawn = Game.spawns.Spawn1;
+
+    if (RCL >= 1) {
+      if (this.sm.allSlots + this.sm.reserves > Object.keys(Game.creeps).length) {
+        Game.spawns.Spawn1.spawnCreep([WORK, CARRY, MOVE], _.uniqueId('nex#'));
       }
-    });
-  }
+      _.map(Game.creeps, (creep: Creep) => {
+        if (!creep.reserved) {
+          const s: SourceWrapper | null = this.sm.reserveSourceSlot() as SourceWrapper;
+          if (s) {
+            creep.reserved = true;
+            CreepActions.basicCreepHarvesting(creep.id, s);
+          }
+        }
+      });
+    }
+    if (RCL >= 2) {
+      // TODO: construction sites for roads to sources
+      const r = spawn.room;
+      // road sites around the spawn
+      SourceManager.getImmediateSurroudings(spawn.pos)
+      .filter(([x, y]) => r.getTerrain().get(x, y) !== TERRAIN_MASK_WALL)
+      .map(([x, y]) => r.createConstructionSite(x, y, STRUCTURE_ROAD));
+      // road sites to sources
+      this.sm.sources
+      .map(val => Game.getObjectById(val.id) as Source)
+      .map(val => PathFinder.search(spawn.pos, val.pos, { swampCost: 1 }).path)
+      .map(val => val
+        .map(pos => r.createConstructionSite(pos.x, pos.y, STRUCTURE_ROAD)));
+
+      // road sites from controller to spawn
+      PathFinder.search(r.controller?.pos as RoomPosition, spawn.pos, { swampCost: 1 }).path
+      .map(pos => r.createConstructionSite(pos.x, pos.y, STRUCTURE_ROAD));
+
+      // add controller to source road sites if that path is efficient
+      this.sm.sources
+      .map(val => Game.getObjectById(val.id) as Source)
+      .map(val => {
+        const bypass = PathFinder.search(spawn.pos, r.controller?.pos as RoomPosition, { swampCost: 1 });
+        const home = PathFinder.search(spawn.pos, val.pos, { swampCost: 1 });
+        const direct = PathFinder.search(r.controller?.pos as RoomPosition, val.pos, { swampCost: 1 });
+        if (bypass.cost + home.cost > (4.0 / 3.0) * direct.cost) {
+        // if (bypass.cost > direct.cost) {
+          direct.path
+            .map(pos => r.createConstructionSite(pos.x, pos.y, STRUCTURE_ROAD));
+        }
+        return true;
+      });
+      // TODO: construction sites for extensions with A*
+      // TODO: build extensions
+      // TODO: build roads to sources
+    }
     ActionScheduler.get().loop();
   }
 
--- src/managers/SourceManager.ts
+++ src/managers/SourceManager.ts
@@ -6,19 +6,18 @@ class SourceManager {
 
     public allSlots: number;
 
-    public reserve: number;
+    public reserves: number;
 
     public constructor(room: Room) {
         this.sources = room.find(FIND_SOURCES).map(val => new SourceWrapper(val.id, SourceManager.getSlots(val)));
         this.allSlots = this.sources.reduce((total, val) => total + val.maxSlots, 0);
-        this.reserve = 1;
+        this.reserves = this.sources.reduce((total, val) => total + Math.round(val.maxSlots * (2.0 / 3.0)), 0);
     }
 
     public reserveSourceSlot(): SourceWrapper | null{
         for (let i = 0; i < this.sources.length; i++) {
-            // TODO: temporary! you're overloading the sources!
             const s = this.sources[i];
-            if (s.occupiedSlots <= s.maxSlots + Math.round(this.reserve * (2.0 / 3.0) * s.maxSlots)) {
+            if (s.occupiedSlots < s.maxSlots + Math.round((2.0 / 3.0) * s.maxSlots)) {
                 s.occupiedSlots++;
                 return s;
             }
@@ -26,7 +25,7 @@ class SourceManager {
         return null;
     }
 
-    private static getImmediateSurroudings(pos: RoomPosition) {
+    public static getImmediateSurroudings(pos: RoomPosition) {
         const res = [];
         for (let x = -1; x < 2; x++) {
             for (let y = -1; y < 2; y++) if (!(x === 0 && y === 0)) res.push([pos.x + x, pos.y + y]);
