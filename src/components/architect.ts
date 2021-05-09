7fbddf56e45dbfa582f537ce20230ceeb1224293
nexovec <hraccesky@gmail.com>
2021-05-09 16:58:02 +0200

    building roads to controller

--- src/components/architect.ts
+++ src/components/architect.ts
@@ -16,28 +16,47 @@ const architect = {
         const x = spawnPos.x - 1 + (i % 3);
         const y = spawnPos.y - 1 + Math.floor(i / 3);
         if (roomTerrain.get(x, y) !== TERRAIN_MASK_WALL) {
-          if (room.lookForAt(LOOK_STRUCTURES, x, y).length)done += 1;
+          if (room.lookForAt(LOOK_STRUCTURES, x, y).length)done++;
           else room.createConstructionSite(x, y, STRUCTURE_ROAD);
-        } else done += 1;
+        } else done++;
       }
-      const isDoneroadAroundSpawn = done === 8;
+      const isDoneRoadAroundSpawn = done === 8;
       // build road from spawn to sources
-      if (isDoneroadAroundSpawn) {
-        let built = 0;
-        let roadLength = 0;
+      let roadLength = 0;
+      let built = 0;
+      if (isDoneRoadAroundSpawn) {
         const spawn = room.find(FIND_MY_SPAWNS)[0];
         room.find(FIND_SOURCES).forEach(src => {
-          const { controller } = room;
           const path = PathFinder.search(spawn.pos, { pos: src.pos, range: 1 });
           roadLength += path.path.length;
           path.path.forEach(pos => {
             if (pos === spawn.pos) return;
-            if (room.lookForAt(LOOK_STRUCTURES, pos.x, pos.y).length)built += 1;
+            const found = room.lookForAt(LOOK_STRUCTURES, pos.x, pos.y);
+            if (found.length)built++;
             else room.createConstructionSite(pos.x, pos.y, STRUCTURE_ROAD);
           });
         });
-        const isDoneRoadsToSources = roadLength === built;
-      }
+      } else return [];
+      const isDoneRoadsToSources = roadLength === built;
+      // build road from spawn to controller
+      roadLength = 0;
+      built = 0;
+      if (isDoneRoadsToSources) {
+        const controller: StructureController = room.controller as StructureController;
+        const path = PathFinder.search(spawnPos, { pos: controller.pos, range: 1 });
+        roadLength += path.path.length;
+        path.path.forEach(pos => {
+          const spawn = room.find(FIND_MY_SPAWNS)[0];
+          if (pos === spawn.pos) return;
+          const found = room.lookForAt(LOOK_STRUCTURES, pos.x, pos.y);
+          if (found.length) {
+            console.log(room.lookForAt(LOOK_STRUCTURES, pos.x, pos.y)[0]);
+            built++;
+          } else room.createConstructionSite(pos.x, pos.y, STRUCTURE_ROAD);
+        });
+      } else return [];
+      const isDoneRoadToController = built === roadLength;
+      // console.log(`${isDoneRoadToController}`);
     }
     return [];
   }
